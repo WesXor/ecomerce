57c3f82cdf471563c6cc50186b237aa7ce72d646
Wesley Costa Viana <wesleyc.viana@gmail.com>
2021-07-13 15:57:12 -0300

    criando a pageadmin e tpladmin

--- index.php
+++ index.php
@@ -3,24 +3,26 @@
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use \Hcode\DB\Sql;
+// use \Hcode\DB\Sql;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	// echo "OK";
-	// $sql = new Sql();
-	// $results = $sql->select("SELECT * FROM tb_users");
-	// echo json_encode($results);
-
 	$page = new  Page();
 	$page->setTpl("index");
 
 });
 
+$app->get('/admin/', function() {
+    
+	$page = new  PageAdmin();
+	$page->setTpl("index");
+
+});
 $app->run();
 
  ?>
